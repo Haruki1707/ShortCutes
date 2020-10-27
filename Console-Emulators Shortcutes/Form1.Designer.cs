4d35a9c7dcce640a25b12fa6008cf84b304dd93e
Haruki1707 <72423267+Haruki1707@users.noreply.github.com>
2020-10-26 22:33:09 -0600

    creation of basics

---
+++ Console-Emulators Shortcutes/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Console_Emulators_Shortcutes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Gdirbox = new System.Windows.Forms.TextBox();
+            this.Edirbox = new System.Windows.Forms.TextBox();
+            this.emulatorcb = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 91);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Gdirbox
+            // 
+            this.Gdirbox.Location = new System.Drawing.Point(114, 43);
+            this.Gdirbox.Name = "Gdirbox";
+            this.Gdirbox.Size = new System.Drawing.Size(254, 20);
+            this.Gdirbox.TabIndex = 1;
+            // 
+            // Edirbox
+            // 
+            this.Edirbox.Location = new System.Drawing.Point(115, 89);
+            this.Edirbox.Name = "Edirbox";
+            this.Edirbox.Size = new System.Drawing.Size(253, 20);
+            this.Edirbox.TabIndex = 2;
+            // 
+            // emulatorcb
+            // 
+            this.emulatorcb.FormattingEnabled = true;
+            this.emulatorcb.Items.AddRange(new object[] {
+            "CEMU",
+            "PJ64",
+            "SNES9X"});
+            this.emulatorcb.Location = new System.Drawing.Point(115, 128);
+            this.emulatorcb.Name = "emulatorcb";
+            this.emulatorcb.Size = new System.Drawing.Size(187, 21);
+            this.emulatorcb.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(438, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(314, 412);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.emulatorcb);
+            this.Controls.Add(this.Edirbox);
+            this.Controls.Add(this.Gdirbox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Gdirbox;
+        private System.Windows.Forms.TextBox Edirbox;
+        private System.Windows.Forms.ComboBox emulatorcb;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Console-Emulators Shortcutes/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using Microsoft.CSharp;
+
+namespace Console_Emulators_Shortcutes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string emu = "";
+            if(emulatorcb.SelectedItem != null)
+            {
+                emu = emulatorcb.SelectedItem.ToString();
+            }
+            
+            string code = Emulator(emu, Gdirbox.Text, Edirbox.Text);
+
+            if(code == "false")
+            {
+                return;
+            }
+
+            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+            ICodeCompiler icc = codeProvider.CreateCompiler();
+            string Output = "Out.exe";
+            Button ButtonObject = (Button)sender;
+
+            CompilerParameters parameters = new CompilerParameters(new[] {"mscorlib.dll", "System.Core.dll", "System.dll"} );
+            //Make sure we generate an EXE, not a DLL
+            parameters.GenerateExecutable = true;
+            parameters.OutputAssembly = Output;
+            CompilerResults results = icc.CompileAssemblyFromSource(parameters, code);
+
+            if (results.Errors.Count > 0)
+            {
+                string errors = null;
+                foreach (CompilerError CompErr in results.Errors)
+                {
+                    errors = errors +
+                                "Line number " + CompErr.Line +
+                                ", Error Number: " + CompErr.ErrorNumber +
+                                ", '" + CompErr.ErrorText + ";" +
+                                Environment.NewLine + Environment.NewLine;
+                }
+                Error(errors);
+            }
+            else
+            {
+                //Successful Compile
+                Succes("Success!");
+                //If we clicked run then launch our EXE
+                //if (ButtonObject.Text == "Run") Process.Start(Output);
+            }
+        }
+
+        private string Emulator(string option, string gamedir, string emulatordir)
+        {
+            string code = "using System;\n" +
+                          "using System.Diagnostics;\n" +
+                          "namespace Emulator_Shortcuts\n" +
+                          "{\n" + 
+                            "class Program\n" +
+                            "{\n" +
+                                "static void Main()\n" +
+                                "{\n";
+            
+            StringComparison comp = StringComparison.OrdinalIgnoreCase;
+            if (gamedir.Contains(emulatordir, comp))
+            {
+                gamedir = gamedir.Replace(emulatordir + @"\", @"");
+                gamedir = gamedir.Replace(@"\", @"\\");
+                emulatordir = @"..\\";
+            }
+            else
+            {
+                Error("Rom's (file or folder) must be in the same directory of the emulator\n" + 
+                    "Example:\n" + 
+                    "  Emulator directory:" + emulatordir + "\n" +
+                    "  Rom's directory:"  + emulatordir + "\\roms");
+                return "false";
+            }
+
+            switch (option)
+            {
+                case "CEMU":
+                    emulatordir += "cemu.exe";
+
+                    code += "Process Cemugame = new Process();\n"+
+                        "Cemugame.StartInfo.FileName = \"" + emulatordir + "\";\n" +
+                        "Cemugame.StartInfo.Arguments = \"-g \\\""+gamedir+"\\\" -f\";" +
+                        "Cemugame.Start();\n";
+                    break;
+                default:
+                    Error("Please select a emulator!");
+                    code = "false";
+                    break;
+            }
+
+            if(code != "false")
+            {
+                code += "}\n" +
+                    "}\n" +
+                "}\n";
+            }
+
+            MessageBox.Show(code);
+            return code;
+        }
+
+        private void Info(string message)
+        {
+            MessageBox.Show(message, "Fix", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void Error(string message)
+        {
+            MessageBox.Show(message, "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void Succes(string message)
+        {
+            MessageBox.Show(message, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+
+    public static class StringExtensions
+    {
+
+        // defines a String extension method 
+        // which includes a StringComparison parameter 
+        public static bool Contains(this String str,
+                                    String substr,
+                                    StringComparison cmp)
+        {
+            if (substr == null)
+                throw new ArgumentNullException("substring substring",
+                                                " cannot be null.");
+
+            else if (!Enum.IsDefined(typeof(StringComparison), cmp))
+                throw new ArgumentException("comp is not a member of",
+                                            "StringComparison, comp");
+
+            return str.IndexOf(substr, cmp) >= 0;
+        }
+    }
+}
